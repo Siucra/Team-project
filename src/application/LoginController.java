9a8cba9844ce584272b3f73e6b29af2245696087
safiatdao <107083240+safiatdao@users.noreply.github.com>
2026-03-10 22:49:55 +0000

    validations
    user email and password validation. Arraylist list created to store log in details

--- src/application/LoginController.java
+++ src/application/LoginController.java
@@ -1,27 +1,72 @@
 package application;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.PasswordField;
+
+import java.io.IOException;
+
+//import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
 
 public class LoginController {
 
     @FXML
-    private TextField userEmail;
+    private TextField loginEmail;
+    
+    @FXML
+    private PasswordField loginPassword;
 
     @FXML
     private Label wrongLogin;
+    
+	//@FXML
+	//private Label passwordEmpty;
+	
+	//@FXML
+	//private Button openSignUpButton;
 
     @FXML
     private void handleLogin() {
     	
-        String email = userEmail.getText();
+        String email = loginEmail.getText();
+        String password = loginPassword.getText();
 
         if (email == null || email.isEmpty()) {
             wrongLogin.setText("Please enter an email.");
-        } else {
-            wrongLogin.setText("Login is successful!");
-            System.out.println("Email entered: " + email);
+            return;
+        } 
+        
+        
+        if(password == null || password.isEmpty()) {
+        	wrongLogin.setText("Please enter a password");
+        	return;
         }
+        	
+        wrongLogin.setText("Login successful!");
+        
+     
+    }
+    
+    @FXML
+    private void openSignUp(ActionEvent e) {
+    	
+    	try {
+    		Parent root = FXMLLoader.load(getClass().getResource("SignUpPage.fxml"));
+    		Scene scene = new Scene(root);
+    		Stage stage = (Stage)((Node) e.getSource()).getScene().getWindow();
+    		stage.setScene(scene);
+    		stage.show();
+    		
+    	} catch(IOException ev) {
+    		ev.printStackTrace();
+    	}
+    	
     }
 }
--- src/application/SignUpController.java
+++ src/application/SignUpController.java
@@ -1,11 +1,21 @@
 package application;
 
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import javafx.scene.control.PasswordField;
+import java.util.ArrayList;
 
 public class SignUpController {
+	
 
 	@FXML
 	private TextField signUpEmail;
@@ -16,8 +26,10 @@ public class SignUpController {
 	@FXML
 	private Label messageLbl;
 	
+	public static ArrayList<userLogin> usersdetails = new ArrayList<>();
+	
 	@FXML
-	    private void handleSignUp(){
+	    private void start(){
 		 
 		  String email = signUpEmail.getText();
 		  String password = signUpPassword.getText();
@@ -27,17 +39,44 @@ public class SignUpController {
 	        	return;
 	        } 
 	        
+	        if(!email.contains("@") || !email.contains(".")) {
+	        	messageLbl.setText("Sorry, your doesn't contain @ and .");
+	        	return;
+	        }
+	        
 	        
 	        if(password == null || password.isEmpty()) {
 	        	messageLbl.setText("Please create a password");
 	        	return;
 	        } 
+	        
+	        if(password.length() < 6) {
+	        	messageLbl.setText("Sorry, passowrd must have at least 6 characters");
+	        	return;
+	        }
 	        	
-	        messageLbl.setText("Strong Password");
-	        System.out.println("Password " + email);
+	        	userLogin newUserLogin = new userLogin(email, password);
+	        	usersdetails.add(newUserLogin);
+	        	
+	        	messageLbl.setText("Account created");
+	       
 	        }
 	        
-	     
+	 @FXML
+	    private void handleSignUp(ActionEvent e) {
+	    	
+	    	try {
+	    		Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+	    		Scene scene = new Scene(root);
+	    		Stage stage = (Stage)((Node) e.getSource()).getScene().getWindow();
+	    		stage.setScene(scene);
+	    		stage.show();
+	    		
+	    	} catch(IOException ev) {
+	    		ev.printStackTrace();
+	    	}
+	    	
+	    }
 
 	 }
 
