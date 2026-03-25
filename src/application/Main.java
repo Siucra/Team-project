38bd482b2d0877f3ba0c6f59fe957ba5fa5b1073
safiatdao <107083240+safiatdao@users.noreply.github.com>
2026-03-25 10:17:55 +0000

    version21

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,9 +12,9 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			//CHANGE LATER
-			Parent root = FXMLLoader.load(getClass().getResource("Subjects.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
