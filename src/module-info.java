87e1cfa590a629d5dae4b64e162e6876b121cef7
xivicece <116080450+xivicece@users.noreply.github.com>
2026-03-30 19:06:54 +0100

    Revert "Merge branch 'CM-branch'"
    This reverts commit ad3ee7678f2d330ddc30e4756f507e88816424d2, reversing
    changes made to 4c98e728ee1909a7add80f31fefd1082b4d828d8.

--- src/module-info.java
+++ src/module-info.java
@@ -4,7 +4,6 @@ module GradeTrackerFX {
 	requires javafx.graphics;
 	requires java.desktop;
 	requires java.sql;
-	requires junit;
 	
 	opens application to javafx.graphics, javafx.fxml;
 	exports application;   
