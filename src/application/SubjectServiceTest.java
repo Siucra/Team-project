71a0e8fa114beec796c1937c49786ca87c4ca1fd
xivicece <116080450+xivicece@users.noreply.github.com>
2026-04-17 18:33:46 +0100

    Commiting the rest of the JUnit tests

--- src/application/SubjectServiceTest.java
+++ src/application/SubjectServiceTest.java
@@ -21,6 +21,54 @@ public class SubjectServiceTest {
         SubjectManager.subjects = new ArrayList<>();
     }
     @Test
+    public void testGetSubjectsReturnsList() {
+        Subject subject1 = new Subject("MATHS", "Higher");
+        Subject subject2 = new Subject("ENGLISH", "Ordinary");
+
+        SubjectManager.subjects.add(subject1);
+        SubjectManager.subjects.add(subject2);
+
+        assertEquals(2, subjectService.getSubjects().size());
+        assertTrue(subjectService.getSubjects().contains(subject1));
+    }
+
+    @Test
+    public void testAddValidSubjectReturnsTrue() {
+        boolean result = subjectService.addSubject("MATHS", "Higher");
+
+        assertTrue(result);
+        assertEquals(1, SubjectManager.subjects.size());
+    }
+
+    @Test
+    public void testAddInvalidSubjectReturnsFalse() {
+        boolean result = subjectService.addSubject("INVALID_SUBJECT", "Higher");
+
+        assertFalse(result);
+        assertEquals(0, SubjectManager.subjects.size());
+    }
+
+    @Test
+    public void testAddDuplicateSubjectReturnsFalse() {
+        SubjectManager.subjects.add(new Subject("MATHS", "Higher"));
+
+        boolean result = subjectService.addSubject("MATHS", "Ordinary");
+
+        assertFalse(result);
+    }
+
+    @Test
+    public void testAddMoreThanSevenSubjectsFails() {
+        for (int i = 0; i < 7; i++) {
+            SubjectManager.subjects.add(new Subject("SUB" + i, "Higher"));
+        }
+
+        boolean result = subjectService.addSubject("MATHS", "Higher");
+
+        assertFalse(result);
+    }
+
+    @Test
     public void testDeleteSubjectRemovesIt() {
         Subject subject = new Subject("MATHS", "Higher");
         SubjectManager.subjects.add(subject);
@@ -29,4 +77,5 @@ public class SubjectServiceTest {
 
         assertEquals(0, SubjectManager.subjects.size());
     }
+}
 }
